5e55c8de3b8a8ac6f23034b0f46480b6341e1d2e
MsZoezo <zoey@zoezo.dev>
2024-06-09 01:20:57 +0200

    Wowie we have a desktop app now~

---
+++ application/src/frontend/app.tsx
@@ -0,0 +1,12 @@
+import "./app.css";
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+
+root.render(
+    <React.StrictMode>
+        <h1>Hello world!</h1>
+    </React.StrictMode>
+);
